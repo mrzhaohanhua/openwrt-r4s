be4a123810b373fa60fd75990a065904d53ca9c5
Zhao Hanhua <40707495+mrzhaohanhua@users.noreply.github.com>
2022-03-19 20:15:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,10 +16,3 @@ svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/tar
 ## 更换target后kmod会有问题
 rm -rf package/kernel/linux/modules/video.mk
 wget -P package/kernel/linux/modules/ https://github.com/immortalwrt/immortalwrt/raw/openwrt-21.02/package/kernel/linux/modules/video.mk
-
-# 更换为 ImmortalWrt的uboot
-# rm -rf package/boot/uboot-rockchip
-# svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/boot/uboot-rockchip package/boot/uboot-rockchip
-# svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/boot/arm-trusted-firmware-rockchip-vendor package/boot/arm-trusted-firmware-rockchip-vendor
-
-
