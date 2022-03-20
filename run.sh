d368edebe2001902763f0efe628f08881d665f4f
Zhao Hanhua <40707495+mrzhaohanhua@users.noreply.github.com>
2022-03-20 09:20:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -96,52 +96,52 @@ svn export https://github.com/mrzhaohanhua/openwrt-package/trunk/luci-theme-neob
 
 ### 后续修改 ###
 
-# 最大连接数
+# 最大连接数（来自QiuSimons/YAOF）
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-#convert_translation
-po_file="$({ find |grep -E "[a-z0-9]+\.zh\-cn.+po"; } 2>"/dev/null")"
-for a in ${po_file}
-do
-	[ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
-	po_new_file="$(echo -e "$a"|sed "s/zh-cn/zh_Hans/g")"
-	mv "$a" "${po_new_file}" 2>"/dev/null"
+#convert_translation（来自QiuSimons/YAOF）
+po_file="$({ find | grep -E "[a-z0-9]+\.zh\-cn.+po"; } 2>"/dev/null")"
+for a in ${po_file}; do
+  [ -n "$(grep "Language: zh_CN" "$a")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$a"
+  po_new_file="$(echo -e "$a" | sed "s/zh-cn/zh_Hans/g")"
+  mv "$a" "${po_new_file}" 2>"/dev/null"
 done
 
-po_file2="$({ find |grep "/zh-cn/" |grep "\.po"; } 2>"/dev/null")"
-for b in ${po_file2}
-do
-	[ -n "$(grep "Language: zh_CN" "$b")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$b"
-	po_new_file2="$(echo -e "$b"|sed "s/zh-cn/zh_Hans/g")"
-	mv "$b" "${po_new_file2}" 2>"/dev/null"
+po_file2="$({ find | grep "/zh-cn/" | grep "\.po"; } 2>"/dev/null")"
+for b in ${po_file2}; do
+  [ -n "$(grep "Language: zh_CN" "$b")" ] && sed -i "s/Language: zh_CN/Language: zh_Hans/g" "$b"
+  po_new_file2="$(echo -e "$b" | sed "s/zh-cn/zh_Hans/g")"
+  mv "$b" "${po_new_file2}" 2>"/dev/null"
 done
 
-lmo_file="$({ find |grep -E "[a-z0-9]+\.zh_Hans.+lmo"; } 2>"/dev/null")"
-for c in ${lmo_file}
-do
-	lmo_new_file="$(echo -e "$c"|sed "s/zh_Hans/zh-cn/g")"
-	mv "$c" "${lmo_new_file}" 2>"/dev/null"
+lmo_file="$({ find | grep -E "[a-z0-9]+\.zh_Hans.+lmo"; } 2>"/dev/null")"
+for c in ${lmo_file}; do
+  lmo_new_file="$(echo -e "$c" | sed "s/zh_Hans/zh-cn/g")"
+  mv "$c" "${lmo_new_file}" 2>"/dev/null"
 done
 
-lmo_file2="$({ find |grep "/zh_Hans/" |grep "\.lmo"; } 2>"/dev/null")"
-for d in ${lmo_file2}
-do
-	lmo_new_file2="$(echo -e "$d"|sed "s/zh_Hans/zh-cn/g")"
-	mv "$d" "${lmo_new_file2}" 2>"/dev/null"
+lmo_file2="$({ find | grep "/zh_Hans/" | grep "\.lmo"; } 2>"/dev/null")"
+for d in ${lmo_file2}; do
+  lmo_new_file2="$(echo -e "$d" | sed "s/zh_Hans/zh-cn/g")"
+  mv "$d" "${lmo_new_file2}" 2>"/dev/null"
 done
 
-po_dir="$({ find |grep "/zh-cn" |sed "/\.po/d" |sed "/\.lmo/d"; } 2>"/dev/null")"
-for e in ${po_dir}
-do
-	po_new_dir="$(echo -e "$e"|sed "s/zh-cn/zh_Hans/g")"
-	mv "$e" "${po_new_dir}" 2>"/dev/null"
+po_dir="$({ find | grep "/zh-cn" | sed "/\.po/d" | sed "/\.lmo/d"; } 2>"/dev/null")"
+for e in ${po_dir}; do
+  po_new_dir="$(echo -e "$e" | sed "s/zh-cn/zh_Hans/g")"
+  mv "$e" "${po_new_dir}" 2>"/dev/null"
 done
 
-makefile_file="$({ find|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
-for f in ${makefile_file}
-do
-	[ -n "$(grep "zh-cn" "$f")" ] && sed -i "s/zh-cn/zh_Hans/g" "$f"
-	[ -n "$(grep "zh_Hans.lmo" "$f")" ] && sed -i "s/zh_Hans.lmo/zh-cn.lmo/g" "$f"
+makefile_file="$({ find | grep Makefile | sed "/Makefile./d"; } 2>"/dev/null")"
+for f in ${makefile_file}; do
+  [ -n "$(grep "zh-cn" "$f")" ] && sed -i "s/zh-cn/zh_Hans/g" "$f"
+  [ -n "$(grep "zh_Hans.lmo" "$f")" ] && sed -i "s/zh_Hans.lmo/zh-cn.lmo/g" "$f"
+done
+
+makefile_file="$({ find package | grep Makefile | sed "/Makefile./d"; } 2>"/dev/null")"
+for g in ${makefile_file}; do
+  [ -n "$(grep "golang-package.mk" "$g")" ] && sed -i "s,\../..,\$(TOPDIR)/feeds/packages,g" "$g"
+  [ -n "$(grep "luci.mk" "$g")" ] && sed -i "s,\../..,\$(TOPDIR)/feeds/luci,g" "$g"
 done
 
 # Remove upx commands
